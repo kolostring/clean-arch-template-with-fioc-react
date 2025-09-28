9dcc75dadf6e75268f5a168b3463a839d03d69f2
kolostring <yoerodriguezjunior@gmail.com>
2025-09-28 14:17:36 -0400

    fix: any type preventing build in DIConfigProvider

--- src/ioc/DIConfigProvider.tsx
+++ src/ioc/DIConfigProvider.tsx
@@ -1,8 +1,13 @@
 "use client";
 import { DependenciesProvider } from "fioc-react";
 import { DI_MANAGER } from "./DI_Manager";
+import { ReactNode } from "react";
 
-export default function DIConfigProvider({ children }: { children: any }) {
+export default function DIConfigProvider({
+  children,
+}: {
+  children: ReactNode | ReactNode[];
+}) {
   return (
     <DependenciesProvider manager={DI_MANAGER}>{children}</DependenciesProvider>
   );
